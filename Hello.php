76034e448e61aeb6cbf0e1a4d33d5e749197210e
francesc <ffores93@gmail.com>
2015-10-06 19:44:08 +0200

    Composer

---
+++ Hello.php
@@ -0,0 +1,8 @@
+<?php
+
+class Hello
+{
+    public  function sayHello(){
+        echo "hello world\n";
+    }
+}
---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__."/Hello.php";
+
+$greeting = new
+Hello();
+$greeting->sayHello();
